35049eb896c22120abff55e8562370432c433171
Zhenia Pasko <advokat.tm@gmail.com>
2018-04-05 18:54:00 +0300

    Initial Commit

---
+++ GitHubUsers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitHubUsers
+//
+//  Created by Yevgenii Pasko on 4/5/18.
+//  Copyright © 2018 Yevgenii Pasko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
